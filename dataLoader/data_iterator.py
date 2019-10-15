c63d077d6ba7b5d5e0338d89101c3abf55a37371
mdeblaauw <mdeblaauw@mobiquityinc.com>
2019-10-15 14:46:56 +0200

    add dataloader to main structure

---
+++ dataLoader/data_iterator.py
@@ -0,0 +1,74 @@
+import random
+import os
+
+import numpy as np
+import pandas as pd
+
+import torch
+import torchaudio
+from torch.utils.data import Dataset
+
+class SequenceDataset(Dataset):
+    def __init__(self, path, min_seq, max_seq, downsampling):
+        self.min_seq = min_seq
+        self.max_seq = max_seq
+        self.downsampling = downsampling
+        
+        self.df = pd.DataFrame(self.index_subset(path))
+
+        # Index of dataframe has direct correspondence to item in dataset
+        self.df = self.df.assign(id=self.df.index.values)
+
+        # Convert arbitrary class names of dataset to ordered 0-(num_speakers - 1) integers
+        self.unique_characters = sorted(self.df['class_name'].unique())
+        self.class_name_to_id = {self.unique_characters[i]: i for i in range(self.num_classes())}
+        self.df = self.df.assign(class_id=self.df['class_name'].apply(lambda c: self.class_name_to_id[c]))
+
+        # Create dicts
+        self.datasetid_to_filepath = self.df.to_dict()['filepath']
+        self.datasetid_to_class_id = self.df.to_dict()['class_id']
+        
+    def __getitem__(self, item):
+        sample, samplerate = torchaudio.load(self.datasetid_to_filepath[item])
+
+        #random sequence length between min and max length
+        if (int)(sample.shape[-1]/samplerate) >= self.max_seq:
+            seq_length = (int)(random.uniform(self.min_seq, self.max_seq)*samplerate)
+        
+            index = random.randrange(0,sample.shape[-1]-seq_length)
+            sample = sample[:,index:index+seq_length]
+        
+        #add padding at the end to make max length sequence
+        padding = torch.zeros(self.max_seq*samplerate - sample.shape[-1]).unsqueeze(0)
+        sample = torch.cat((sample,padding),1)
+
+        #downsample kHz
+        sample = sample[:,::self.downsampling]
+        
+        label = self.datasetid_to_class_id[item]
+        return(sample,label)
+        
+    def __len__(self):
+        return(len(self.df))
+
+    def num_classes(self):
+        return(len(self.df['class_name'].unique()))
+    
+    @staticmethod
+    def index_subset(path):
+        samples = []
+        for root, folders, files in os.walk(path):
+            if not files:
+                continue;
+            
+            alphabet = root.split('/')[-2]
+            class_name = root.split('/')[-1]
+            
+            for file in files:
+                samples.append({
+                    'alphabet': alphabet,
+                    'class_name': class_name,
+                    'filepath': os.path.join(root, file)
+                })
+                
+        return(samples)
---
+++ main.py
@@ -0,0 +1,3 @@
+from experiments.test_dataloader import bla
+
+bla()
