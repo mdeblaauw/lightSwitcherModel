d39b135125668c5bbdf248180a51c0a77f0ec8f6
mdeblaauw <mdeblaauw@mobiquityinc.com>
2019-11-11 11:27:55 +0100

    add progressbar for dataloaders

--- experiments/proto_net.py
+++ experiments/proto_net.py
@@ -22,6 +22,7 @@ ex = Experiment(EXPERIMENT_NAME)
 
 USE_CUDA = torch.cuda.is_available()
 device = torch.device("cuda" if USE_CUDA else "cpu")
+print('device that will be used is', device)
 
 class ProtoTrainer():
     def __init__(self):
